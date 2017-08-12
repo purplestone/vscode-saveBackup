1eb896221f959bdd35d6cc4190a8ff7487f4462b
purplestone <gaoyuan.ggg@gmail.com>
2017-08-12 22:50:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# saveBackup README
+
+This is a extension for backup file when you save.
+
+## Features
+
+When you save, Current file text that open will copy to directory that configure by `saveBackup.conf.backupDir`
+
+> `saveBackup.conf.backupDir` default set to `USER_DATA_DIR/.vscode/saveBackup`
+
+## Extension Settings
+
+* `saveBackup.conf.enable`: enable/disable this extension
+* `saveBackup.conf.backupDir`: backup directory
+* `saveBackup.conf.fileNameMatch`: match fileName to backup
+
+
---
+++ extension.js
@@ -0,0 +1,73 @@
+
+var vscode = require('vscode');
+var $$path = require('path');
+var $$fs = require('fs');
+
+function activate(context) {
+    // console.log('Congratulations, your extension "savebackup" is now active!');
+
+    var disposable = vscode.commands.registerTextEditorCommand('extension.saveBackup.backupFile', function (textEditor, edit) {
+        backupFile(textEditor.document);
+    });
+    vscode.workspace.onDidSaveTextDocument(function (document) {
+        backupFile(document);
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+function backupFile(document) {
+    var sFileText = document.getText();
+    var sFilePath = document.uri.path;
+    if (sFilePath[0] === '/') {
+        sFilePath = sFilePath.slice(1); 
+    }
+    var oConf = vscode.workspace.getConfiguration('saveBackup.conf');
+
+    if (oConf.enable) {
+        var sBakPath = buildeBakPath(sFilePath, oConf.backupDir);
+        
+        if ($$fs.existsSync(oConf.backupDir)) {
+            try {
+                var sFileName = $$path.basename(sFilePath);
+                if (new RegExp(oConf.fileNameMatch).test(sFileName)) {
+                    var sFileDir = $$path.dirname(sBakPath);
+                    if (!$$fs.existsSync(sFileDir)) {
+                        $$fs.mkdirSync(sFileDir);
+                    }
+                    $$fs.writeFileSync(sBakPath, sFileText); 
+                }
+            } catch (error) {
+                vscode.window.showErrorMessage(`extension.saveBackup : ${error.message}`);
+            }
+        }else{
+            vscode.window.showErrorMessage(`extension.saveBackup : ${oConf.backupDir} is not exists. To create the dir, or configure saveBackup.conf.backupDir`);
+        }
+    }
+}
+
+function buildeBakPath(sFilePath, sBackupDir) {
+    var sVscodeDir = $$path.join(__dirname, '../..'); 
+    sBackupDir = sBackupDir.replace('${.vscode}', sVscodeDir);
+    var sNewPath = sFilePath.replace(/[\/:]/g, '_');
+    var sR = $$path.join(sBackupDir, sNewPath);
+    var sExName = $$path.extname(sFilePath);
+    var oD = new Date(); 
+    var sTime = `${oD.getFullYear()}${c2(oD.getMonth())}${c2(oD.getDay())}`;
+    sTime += `_${c2(oD.getHours())}${c2(oD.getMinutes())}${c2(oD.getSeconds())}` + '_' + (+oD).toString().slice(-3);
+    sR = $$path.join(sR, sTime + sExName).replace(/\\/g, '/');
+
+    return sR;
+}
+function c2(n) {
+    return (n/100).toFixed(2).slice(-2);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
+
+
+
+
