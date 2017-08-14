61a1b7f5c6ff0d499815462df5bd9b526cfe6ff2
衍兮 <yanxi.gy@alibaba-inc.com>
2017-08-14 14:22:45 +0800

    add logo;fixed month bug;errorTip real pah

--- extension.js
+++ extension.js
@@ -25,9 +25,9 @@ function backupFile(document) {
     var oConf = vscode.workspace.getConfiguration('saveBackup.conf');
 
     if (oConf.enable) {
-        var sBakPath = buildeBakPath(sFilePath, oConf.backupDir);
-        
-        if ($$fs.existsSync(oConf.backupDir)) {
+        var backupDir = getParsePath(oConf.backupDir);
+        var sBakPath = buildeBakPath(sFilePath, backupDir);
+        if ($$fs.existsSync(backupDir)) {
             try {
                 var sFileName = $$path.basename(sFilePath);
                 if (new RegExp(oConf.fileNameMatch).test(sFileName)) {
@@ -41,23 +41,26 @@ function backupFile(document) {
                 vscode.window.showErrorMessage(`extension.saveBackup : ${error.message}`);
             }
         }else{
-            vscode.window.showErrorMessage(`extension.saveBackup : ${oConf.backupDir} is not exists. To create the dir, or configure saveBackup.conf.backupDir`);
+            vscode.window.showErrorMessage(`extension.saveBackup : ${backupDir} is not exists. To create the dir, or configure saveBackup.conf.backupDir`);
         }
     }
 }
 
 function buildeBakPath(sFilePath, sBackupDir) {
-    var sVscodeDir = $$path.join(__dirname, '../..'); 
-    sBackupDir = sBackupDir.replace('${.vscode}', sVscodeDir);
     var sNewPath = sFilePath.replace(/[\/:]/g, '_');
     var sR = $$path.join(sBackupDir, sNewPath);
     var sExName = $$path.extname(sFilePath);
     var oD = new Date(); 
-    var sTime = `${oD.getFullYear()}${c2(oD.getMonth())}${c2(oD.getDay())}`;
+    var sTime = `${oD.getFullYear()}${c2(oD.getMonth()+1)}${c2(oD.getDay())}`;
     sTime += `_${c2(oD.getHours())}${c2(oD.getMinutes())}${c2(oD.getSeconds())}` + '_' + (+oD).toString().slice(-3);
     sR = $$path.join(sR, sTime + sExName).replace(/\\/g, '/');
 
     return sR;
+}
+function getParsePath(sPath) {
+    var sVscodeDir = $$path.join(__dirname, '../..'); 
+    sPath = sPath.replace('${.vscode}', sVscodeDir);
+    return sPath.replace(/\\/g, '/');
 }
 function c2(n) {
     return (n/100).toFixed(2).slice(-2);
