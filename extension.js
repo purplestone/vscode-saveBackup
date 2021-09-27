fd99c9f1147049822eb957fb5ec256988e833b54
Max Markon <mm@aps-delta.de>
2021-09-27 16:27:49 +0200

    added option to specify a RegEx modifier to File Name Match

--- extension.js
+++ extension.js
@@ -1,4 +1,3 @@
-
 var vscode = require('vscode');
 var $$path = require('path');
 var $$fs = require('fs');
@@ -6,10 +5,10 @@ var $$fs = require('fs');
 function activate(context) {
     // console.log('Congratulations, your extension "savebackup" is now active!');
 
-    var disposable = vscode.commands.registerTextEditorCommand('extension.saveBackup.backupFile', function (textEditor, edit) {
+    var disposable = vscode.commands.registerTextEditorCommand('extension.saveBackup.backupFile', function(textEditor, edit) {
         backupFile(textEditor.document);
     });
-    vscode.workspace.onDidSaveTextDocument(function (document) {
+    vscode.workspace.onDidSaveTextDocument(function(document) {
         backupFile(document);
     });
 
@@ -20,7 +19,7 @@ function backupFile(document) {
     var sFileText = document.getText();
     var sFilePath = document.uri.path;
     if (sFilePath[0] === '/') {
-        sFilePath = sFilePath.slice(1); 
+        sFilePath = sFilePath.slice(1);
     }
     var oConf = vscode.workspace.getConfiguration('saveBackup.conf');
 
@@ -30,17 +29,17 @@ function backupFile(document) {
         if ($$fs.existsSync(backupDir)) {
             try {
                 var sFileName = $$path.basename(sFilePath);
-                if (new RegExp(oConf.fileNameMatch).test(sFileName)) {
+                if (new RegExp(oConf.fileNameMatch, oConf.fileNameMatchModifier).test(sFileName)) {
                     var sFileDir = $$path.dirname(sBakPath);
                     if (!$$fs.existsSync(sFileDir)) {
                         $$fs.mkdirSync(sFileDir);
                     }
-                    $$fs.writeFileSync(sBakPath, sFileText); 
+                    $$fs.writeFileSync(sBakPath, sFileText);
                 }
             } catch (error) {
                 vscode.window.showErrorMessage(`extension.saveBackup : ${error.message}`);
             }
-        }else{
+        } else {
             vscode.window.showErrorMessage(`extension.saveBackup : ${backupDir} is not exists. To create the dir, or configure saveBackup.conf.backupDir`);
         }
     }
@@ -50,27 +49,24 @@ function buildeBakPath(sFilePath, sBackupDir) {
     var sNewPath = sFilePath.replace(/[\/:]/g, '_');
     var sR = $$path.join(sBackupDir, sNewPath);
     var sExName = $$path.extname(sFilePath);
-    var oD = new Date(); 
+    var oD = new Date();
     var sTime = `${oD.getFullYear()}${c2(oD.getMonth()+1)}${c2(oD.getDate())}`;
     sTime += `_${c2(oD.getHours())}${c2(oD.getMinutes())}${c2(oD.getSeconds())}` + '_' + (+oD).toString().slice(-3);
     sR = $$path.join(sR, sTime + sExName).replace(/\\/g, '/');
 
     return sR;
 }
+
 function getParsePath(sPath) {
-    var sVscodeDir = $$path.join(__dirname, '../..'); 
+    var sVscodeDir = $$path.join(__dirname, '../..');
     sPath = sPath.replace('${.vscode}', sVscodeDir);
     return sPath.replace(/\\/g, '/');
 }
+
 function c2(n) {
-    return (n/100).toFixed(2).slice(-2);
+    return (n / 100).toFixed(2).slice(-2);
 }
 exports.activate = activate;
 
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
-
-
-
-
